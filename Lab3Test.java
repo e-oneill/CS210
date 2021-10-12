6812d8dc298daf975ef3bda9cd179c7dafab8199
e-oneill <eoneill@live.co.uk>
2021-10-12 16:44:31 +0100

    Added tester

---
+++ Lab3Test.java
@@ -0,0 +1,51 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
+public class Lab3Test {
+//  @Test(dataProvider = "dp")
+//  public void f(Integer n, String s) {
+//  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+
+  @DataProvider (name="notPrime")
+  public Object[][] dp() {
+    return new Object[][] {
+      { 1, 6, 5, 7 },
+      { 2, 9, 7, 11 },
+      { 3, 4, 3, 5},
+      { 4, 1000, 997, 1009}
+    };
+  }
+  
+  @DataProvider (name="Prime")
+  public Object[][] prime() {
+    return new Object[][] {
+      { 1, 7, 11},
+      { 2, 11, 13},
+      { 3, 997, 1009},
+      { 4, 2, 3}
+    };
+  }
+
+  @Test (dataProvider = "notPrime")
+  public void testNotPrime(int id, int bound, int expectedPrev, int expectedSubs) {
+    boolean[] arr = new boolean[bound+21];
+    arr = Lab3.getPrimes(arr);
+    assertEquals(Lab3.findPreviousPrime(bound, arr), expectedPrev);
+    assertEquals(Lab3.findSubsequentPrime(bound, arr), expectedSubs);
+  }
+
+  @Test (dataProvider = "Prime")
+  public void testPrime(int id, int bound, int expectedSubs) {
+	    boolean[] arr = new boolean[bound+21];
+	    arr = Lab3.getPrimes(arr);
+	    assertEquals(Lab3.findSubsequentPrime(bound, arr), expectedSubs);
+  }
+}
