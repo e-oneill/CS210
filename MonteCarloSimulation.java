9b615d2d82a209404fdee80ef50fe42ada4f59ec
e-oneill <eoneill@live.co.uk>
2021-10-19 11:52:13 +0100

    Montecarlo

--- MonteCarloSimulation.java
+++ MonteCarloSimulation.java
@@ -6,35 +6,11 @@ public class MonteCarloSimulation {
 
         int n = sc.nextInt();
         int x = sc.nextInt();
-        int t = 100000;
+        int t = 30000;
         int collisions = 0;
         sc.close();
         for (int r = 0; r < t; r++){
-        //region Array method
-        // int[] pupils = generateRandomBirthdays(n);
-
-        //     for (int b = 0; b < pupils.length; b++)
-        //     {
-        //         int innerCollisions = 0;  
-        //         for (int j = 1; j < pupils.length; j++)
-        //         {
-                  
-        //             if (pupils[j] == pupils[b] && b != j)
-        //             {
-        //                 innerCollisions++;
-        //                 break;
-        //             }
-                    
-        //         }
-        //         if (innerCollisions > 0){
-        //             collisions += innerCollisions;
-        //             break;
-        //         }
-
-        //     }
-        // endregion;
-        
-        //region HashSet method
+        //region ArrayList method
         List<Integer> pupils = new ArrayList<Integer>();
         Random rand = new Random();
         
@@ -46,23 +22,20 @@ public class MonteCarloSimulation {
 
                 if (pupils.contains(integer)){
                     innerCollisions++;
-                
+                    List<Integer> subPupils = new ArrayList<Integer>();
+                    subPupils = pupils.subList(pupils.indexOf(integer)+1, pupils.size());
                     for (int j = 1; j <= x; j++)
                     {
                         if (innerCollisions >= x-1){
-                            
                             collisions++;
-                            // System.out.println("Collision found " + integer);
-                            // pupils.add(integer);
                             break;
                         }
-                        List<Integer> subPupils = new ArrayList<Integer>();
-                        subPupils = pupils.subList(pupils.indexOf(integer)+1, pupils.size());
                         if (subPupils.contains(integer)) {
                             innerCollisions++;
                         } else {
                             break;
                         }
+                        subPupils = subPupils.subList(subPupils.indexOf(integer)+1, subPupils.size());
                     }
                     if (innerCollisions >=x-1 && collisions > 0)
                     {
@@ -72,28 +45,12 @@ public class MonteCarloSimulation {
                 } 
                 pupils.add(integer);
             }
-        
-            // System.out.println(pupils.toString());
         }
 
         float prob = (float)collisions / t;
         int probInt = (int) (prob*100);
-        System.out.println("After " + t + " tests we had " + collisions + " collisions, result in probability of: " + probInt + "%");
+        System.out.println("After " + t + " tests we had " + collisions + " collisions, resulting in a probability of: " + probInt + "%");
 
         
     }
-
-    public static int[] generateRandomBirthdays(int n)
-    {
-        int[] pupils = new int[n];
-        Random rand = new Random();
-
-        for (int i = 0; i < pupils.length; i++)
-        {
-            pupils[i] = rand.nextInt(364);
-            // System.out.print(pupils[i] + " ");
-        }
-        return pupils;
-    }
-
 }
