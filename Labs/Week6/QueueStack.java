59ccf95b192a5697d1a2f4bff30348b089297c92
e-oneill <eoneill@live.co.uk>
2021-11-16 10:40:44 +0000

    Stack with 2 Queues

--- Labs/Week6/QueueStack.java
+++ Labs/Week6/QueueStack.java
@@ -2,19 +2,51 @@ package Labs.Week6;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
+import java.util.Scanner;
 public class QueueStack {
-    public static void main(String[] args) {
-        TwoQueueStack stack = new TwoQueueStack();
+    public static void main(String[] args) 
+    {
+        Queue<Integer> queue1 = new LinkedList<Integer>();
+        Queue<Integer>  queue2 = new LinkedList<Integer>();
+
+        Scanner sc = new Scanner(System.in);
+        int maxSize = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < maxSize; i++)
+        {
+            String command = sc.nextLine().toLowerCase();
+            if (command.equalsIgnoreCase("pop"))
+            {
+                queue1.poll();
+            }
+            else if (command.equalsIgnoreCase("clear"))
+            {
+                queue1.clear();
+            }
+            else if (command.equalsIgnoreCase("peek"))
+            {
+                System.out.println(queue1.peek());
+            }
+            else
+            {
+                int var = Integer.parseInt(command.split(" ")[1]);
+                queue2.addAll(queue1);
+                queue1.clear();
+                queue1.add(var);
+                while (queue2.peek() != null)
+                {
+                queue1.add(queue2.poll());
+                }
+            }
+        }
+        if (queue1.peek() != null)
+        {
+        System.out.println(queue1.peek());
+        }
+        else
+        {
+            System.out.println("empty");  
+        }
     }
 }
 
-class TwoQueueStack {
-    private Queue<Integer> queue1 = new LinkedList<Integer>();
-    private Queue<Integer>  queue2 = new LinkedList<Integer>() {
-        
-    };
-    public TwoQueueStack() {
-
-    }
-}
